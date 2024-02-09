08252141ea62ce435affa9e33b1686c629822ac6
seanth <seanth@gmail.com>
2024-02-08 19:49:05 -0600

    More attempts at using websockets

--- tools/Browser_display/show_model.py
+++ tools/Browser_display/show_model.py
@@ -6,13 +6,13 @@ import random
 import websockets
 
 async def show_time(websocket):
-    fileList=['default0.glb','default1.glb','default2.glb','default3.glb']
+    fileList=['default0.glb','default3.glb']
     while True:
-        # message = datetime.datetime.utcnow().isoformat() + "Z"
-        # message = "default0.glb"
-        message = random.choice(fileList)
-        await websocket.send(message)
-        await asyncio.sleep(random.random() * 2 + 1)
+        aFileName = random.choice(fileList)
+        with open(aFileName, 'rb') as theFile:
+            modelData = theFile.read()
+        await websocket.send(modelData)
+        await asyncio.sleep(5)
 
 async def main():
     async with websockets.serve(show_time, "localhost", 5678):
